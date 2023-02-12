be9eaaae64fd838124da58060ce26b14e8d650d7
Yuta Okuzono <spadeloves@gmail.com>
2023-02-12 21:32:09 +0900

    initial

---
+++ spear.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "spearlyAuthKey": "vHxvGOmH7htBgc88ZJxeZrrtfP8E4HiEPZ9Gpx4CFjI",
+  "projectName": "oxo-tech-site-spear",
+  "generateSitemap": true,
+  "siteURL": "https://oxo-tech.ooo/"
+};
