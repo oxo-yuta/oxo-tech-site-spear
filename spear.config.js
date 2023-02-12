7ed1a4bba7a18d98313c9b624f3bb19ecf5f27ff
Yuta Okuzono <spadeloves@gmail.com>
2023-02-12 21:44:18 +0900

    bring index and compiled css

--- spear.config.js
+++ spear.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "spearlyAuthKey": "vHxvGOmH7htBgc88ZJxeZrrtfP8E4HiEPZ9Gpx4CFjI",
-  "projectName": "oxo-tech-site-spear",
+  "projectName": "OXO-tech",
   "generateSitemap": true,
   "siteURL": "https://oxo-tech.ooo/"
 };
